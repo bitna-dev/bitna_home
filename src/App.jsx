c584dc7d89d84ec8282ae48bb0c0e1c681e4f4ec
Bitna <bitna@Bitnas-MacBook-Pro.local>
2024-06-10 12:30:09 -0700

    convert pnpm to yarn

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,14 @@ import { Routes } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import Home from '@pages/Home'
 import Test from '@pages/Test'
-import Layout from './components/Layout.jsx'
 
 function App() {
   return (
     <>
-      <Layout>
-        <Routes>
-          <Route path="/" Component={Home} />
-          <Route path="/test" Component={Test} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/test" Component={Test} />
+      </Routes>
     </>
   )
 }
